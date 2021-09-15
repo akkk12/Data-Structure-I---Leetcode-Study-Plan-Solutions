5fab494b5f955e21a23b1ac0255b466dc670d4d8
akkk12 <68320786+akkk12@users.noreply.github.com>
2021-09-15 11:22:19 +0530

    Add files via upload

---
+++ Day3.cpp
@@ -0,0 +1,33 @@
+//121. Best Time to Buy and Sell Stock
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+int maxProfit(vector<int>& prices) {
+        int maxPrice = 0 ;
+        int minPrice = (int)1e9 ;
+        int n = prices.size() ;
+        for(int i = 0 ; i < n ;i++){
+            minPrice = min(minPrice,prices[i]);
+            maxPrice = max(maxPrice,prices[i] - minPrice);
+        }
+        return maxPrice ;
+    }
+
+
+// 350. Intersection of Two Arrays II
+//https://leetcode.com/problems/intersection-of-two-arrays-ii/
+// it also has a DP Solution  
+   vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> map ;
+        vector<int> ans ;
+        int n = nums1.size() , m = nums2.size();
+        for(int i = 0 ; i < n ;i++){
+            map[nums1[i]]++ ;
+        }
+        for(int i = 0 ; i < m ; i++){
+            if(map[nums2[i]] > 0) {
+                ans.push_back(nums2[i]);
+                map[nums2[i]]-- ;}
+        }
+        return ans ;
+        
+    }    
